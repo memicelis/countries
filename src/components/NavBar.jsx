f152a7ce3ff68cb0065287612292ffff6c708360
Elis Memic <elismemic5@gmail.com>
2023-10-09 17:23:48 +0200

    Create NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {NavLink} from 'react-router-dom';
+import { FaMicrophone, FaCog, FaChevronLeft } from 'react-icons/fa';
+
+const NavBar = ()=> (
+    <>
+    <nav className="flex justify-between items-center bg-dark p-2">
+    <NavLink to="/">
+        <FaChevronLeft className="text-white" />
+    </NavLink>
+    <h1 className="text-white">Europe</h1>
+    <div className="flex items-center gap-2">
+        <FaMicrophone className="text-white"/>
+        <FaCog className="text-white"/>
+    </div>
+    </nav>
+    </>
+);
+
+export default NavBar;
