659617de7642252529f66150db14f1b35623535a
Elis Memic <elismemic5@gmail.com>
2023-10-09 17:23:18 +0200

    created tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      dark: '#4369b2',
+      light: '#5788e5',
+      'white': '#fefefd',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
