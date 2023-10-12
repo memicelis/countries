63774f8a06327b9d7ec5efe2d1bbdc5fad218e2a
Elis Memic <elismemic5@gmail.com>
2023-10-12 16:23:11 +0200

    Add tests for CountryList, Details, Home

---
+++ src/__test__/components/Details.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux'; // Import the Provider from react-redux
+import store from '../../redux/store'; // Import your Redux store
+import Details from '../../components/CountryList';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+test('renders CountryList without errors', () => {
+     render(
+        <Provider store={store}>
+          <Router>
+            <Details />
+          </Router>
+        </Provider>
+    );
+});
